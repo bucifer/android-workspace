bea540387da9fe523caeebe26ddf89d11326ed98
Terry Bu <ktbucifer@gmail.com>
2014-10-04 15:01:33 -0400

    Got POST request working for create new user

--- Parent-DigitalLeash/src/com/example/parent_digitalleash/MainActivity.java
+++ Parent-DigitalLeash/src/com/example/parent_digitalleash/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.parent_digitalleash;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.ParseException;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 
 
@@ -16,7 +37,10 @@ public class MainActivity extends ActionBarActivity {
 
 	EditText latitudeEditTextField;
 	EditText longitudeEditTextField;
-		
+	EditText radiusEditTextField;
+	EditText usernameEditTextField;
+	Button createNewUserButton;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,9 +56,51 @@ public class MainActivity extends ActionBarActivity {
         
         latitudeEditTextField = (EditText) findViewById(R.id.latitudeEditTextField);
         longitudeEditTextField = (EditText) findViewById(R.id.longitudeEditTextField);
+        radiusEditTextField = (EditText) findViewById(R.id.radiusEditTextField);
+        usernameEditTextField = (EditText) findViewById(R.id.usernameEditTextField);
+        createNewUserButton = (Button) findViewById(R.id.createNewUserbutton);
+        
+        this.startUpdatingLocations();
+        createNewUserButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				
+				if (usernameEditTextField.getText().length() < 2) {
+					AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+		            builder1.setMessage("Username is too short - please input valid username");
+		            builder1.setCancelable(true);
+		            builder1.setPositiveButton("OK",
+		                    new DialogInterface.OnClickListener() {
+		                public void onClick(DialogInterface dialog, int id) {
+		                    dialog.cancel();
+		                }
+		            });
+		            AlertDialog alert11 = builder1.create();
+		            alert11.show();
+				}
+				else if (radiusEditTextField.getText().length() == 0) {
+					AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+		            builder1.setMessage("radius doesn't exist - please input valid radius");
+		            builder1.setCancelable(true);
+		            builder1.setPositiveButton("OK",
+		                    new DialogInterface.OnClickListener() {
+		                public void onClick(DialogInterface dialog, int id) {
+		                    dialog.cancel();
+		                }
+		            });
+		            AlertDialog alert11 = builder1.create();
+		            alert11.show();
+				}
+				else {
+					new PostRequestAsyncTask().execute(); 
+			        Log.d("TERRY","post request done");
+				}
+			}
+		});  
+    }
+    
+    public void startUpdatingLocations() {
         LocationManager manager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         LocationListener listener = new LocationListener() {
-						
 			@Override
 			public void onLocationChanged(Location location) {
 				// TODO Auto-generated method stub
@@ -61,13 +127,73 @@ public class MainActivity extends ActionBarActivity {
 				
 			}
 		};
-        
 		manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
-        
-        
     }
 
+    class PostRequestAsyncTask extends AsyncTask<Void,Void,Void>
+    {
+              protected void onPreExecute()
+              {           super.onPreExecute();
+                        //display progressdialog.
+              } 
 
+               protected Void doInBackground(Void ...params)
+              {  
+                //http request. do not update ui here 
+           		JSONObject jsonobj;
+           		JSONObject user;
+           		try {
+           	        user = new JSONObject();
+           	        user.put("username", usernameEditTextField.getText());
+           	        user.put("latitude", latitudeEditTextField.getText());
+           	        user.put("longitude", longitudeEditTextField.getText());
+           	        user.put("radius", radiusEditTextField.getText());
+           	        jsonobj = new JSONObject();
+           	        jsonobj.put("user", user);
+           	        jsonobj.put("commit", "Create User");
+           	        jsonobj.put("action", "update");
+           	        jsonobj.put("controller","users");
+           			HttpClient httpClient = new DefaultHttpClient();
+           	        HttpPost httpPost = new HttpPost("http://protected-wildwood-8664.herokuapp.com/users");
+           	        StringEntity se = new StringEntity(jsonobj.toString());
+           	        se.setContentType("application/json;charset=UTF-8");
+           	        se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,"application/json;charset=UTF-8"));
+           	        httpPost.setEntity(se);
+           	        HttpResponse httpresponse = httpClient.execute(httpPost);
+           	        String responseText = null;
+           	        try {
+           	        responseText = EntityUtils.toString(httpresponse.getEntity());
+           	        }catch (ParseException e) {
+           	        e.printStackTrace();
+           	        Log.i("Parse Exception", e + "");
+           	        }
+           	        
+           		} catch (JSONException e) {
+           			// TODO Auto-generated catch block
+           			e.printStackTrace();
+           		} catch (UnsupportedEncodingException e) {
+           			// TODO Auto-generated catch block
+           			e.printStackTrace();
+           		} catch (ClientProtocolException e) {
+           			// TODO Auto-generated catch block
+           			e.printStackTrace();
+           		} catch (IOException e) {
+           			// TODO Auto-generated catch block
+           			e.printStackTrace();
+           		}
+
+                    return null;
+              } 
+
+               protected void onPostExecute(Void result)
+              {     
+                        super.onPostExecute(result);
+
+              } 
+
+    }
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
