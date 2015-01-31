1c00abc98cb50b620af1e5207f547a3c08e568ea
Terry Bu <burea1124@gmail.com>
2015-01-30 23:14:44 -0500

    started on big nerd ranch geoquiz first app

---
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/TrueFalse.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.geoquiz;
+
+public class TrueFalse {
+	
+	private int mQuestion;
+	private boolean mTrueQuestion;
+	
+	public TrueFalse(int question, boolean trueQuestion) {
+		mQuestion = question;
+		mTrueQuestion = trueQuestion;
+	}
+}
