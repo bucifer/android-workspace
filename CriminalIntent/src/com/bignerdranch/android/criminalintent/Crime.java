998f847ed8f8a07a98c418805d7aa74f14352c80
Terry Bu <burea1124@gmail.com>
2015-01-31 22:35:49 -0500

    started on Chapter 8 laying out fragment UI for criminal intent

---
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,46 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+	private UUID mID;
+	private String mTitle;
+	private Date mDate;
+	private boolean mSolved;
+	
+	public Crime() {
+		//Generate unique id
+		mID = UUID.randomUUID();
+		mDate = new Date();
+	}
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean solved) {
+		mSolved = solved;
+	}
+
+	public String getTitle() {
+		return mTitle;
+	}
+
+	public void setTitle(String title) {
+		mTitle = title;
+	}
+
+	public UUID getID() {
+		return mID;
+	}
+}	
--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,9 +1,9 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentManager;
 
 
 public class CrimeActivity extends FragmentActivity {
@@ -12,18 +12,18 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        
+        //in this particular app, we are concerned with backwards compatibility with Froyo, Honeycomb, etc
+        //If not, you can use getFragmentManager instead
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+        	fragment = new CrimeFragment();
+        	fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
         }
-        return super.onOptionsItemSelected(item);
+        
+        
     }
+
+
 }
